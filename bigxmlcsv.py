ad5a75b4612736d54be42b409baf63547e31b311
Nicolas Bortolotti <nbortolotti@users.noreply.github.com>
2015-08-08 11:16:34 -0500

    Update bigxmlcsv.py
    cleaning the code and fix autor name

--- bigxmlcsv.py
+++ bigxmlcsv.py
@@ -1,4 +1,4 @@
-__author__ = 'nickbortolotti'
+__author__ = 'nbortolotti'
 
 import csv
 import cStringIO
@@ -26,22 +26,6 @@ class xmlcsv:
         test_file.close()
 
 class UnicodeWriter(object):
-    """
-    Like UnicodeDictWriter, but takes lists rather than dictionaries.
-
-    Usage example:
-
-    fp = open('my-file.csv', 'wb')
-    writer = UnicodeWriter(fp)
-    writer.writerows([
-        [u'Bob', 22, 7],
-        [u'Sue', 28, 6],
-        [u'Ben', 31, 8],
-        # \xc3\x80 is LATIN CAPITAL LETTER A WITH MACRON
-        ['\xc4\x80dam'.decode('utf8'), 11, 4],
-    ])
-    fp.close()
-    """
 
     def __init__(self, f, dialect=csv.excel_tab, encoding="utf-16", **kwds):
         # Redirect output to a queue
@@ -68,26 +52,6 @@ class UnicodeWriter(object):
             self.writerow(row)
 
 class UnicodeDictWriter(UnicodeWriter):
-    """
-    A CSV writer that produces Excel-compatibly CSV files from unicode data.
-    Uses UTF-16 and tabs as delimeters - it turns out this is the only way to
-    get unicode data in to Excel using CSV.
-
-    Usage example:
-
-    fp = open('my-file.csv', 'wb')
-    writer = UnicodeDictWriter(fp, ['name', 'age', 'shoesize'])
-    writer.writerows([
-        {'name': u'Bob', 'age': 22, 'shoesize': 7},
-        {'name': u'Sue', 'age': 28, 'shoesize': 6},
-        {'name': u'Ben', 'age': 31, 'shoesize': 8},
-        # \xc3\x80 is LATIN CAPITAL LETTER A WITH MACRON
-        {'name': '\xc4\x80dam'.decode('utf8'), 'age': 11, 'shoesize': 4},
-    ])
-    fp.close()
-
-    Initially derived from http://docs.python.org/lib/csv-examples.html
-    """
 
     def __init__(self, f, fields, dialect=csv.excel,
                  encoding="utf-8", **kwds):
